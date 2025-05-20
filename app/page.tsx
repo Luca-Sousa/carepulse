05add0064afee8076b499a7a4ba43d047eb2deaf
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-05-20 15:38:09 -0300

    feat: add initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
+import Image from "next/image";
+import PatientForm from "./components/forms/patient-form";
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="Logo"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 Care Pulse
+            </p>
+
+            <Link href="/?admin=true" className="text-green-500">
+              Adminstrador
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="Imagem de Paciente"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 };
